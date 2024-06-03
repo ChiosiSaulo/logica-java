2739e131c4729512ba401c9a78a3c0eab399b071
Saulo Chiosi <sauloc.1234@gmail.com>
2024-06-03 13:46:46 -0300

    Criação

---
+++ src/Debug.java
@@ -0,0 +1,12 @@
+
+
+public class Debug {
+
+	public static void main(String[] args) {
+		for(int i = 4; i >=0; i--) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
---
+++ src/ExercicioCasting.java
@@ -0,0 +1,21 @@
+
+public class ExercicioCasting {
+	
+	public static void main(String[] args) {
+		double x = 5.999;
+		int y;
+		
+		y = (int) x;
+		
+		System.out.println(y);
+		
+		int a, b;
+		double c;
+		a = 5;
+		b = 2;
+		
+		c = (double) a/b;
+		System.out.println(c);
+	
+	}
+}
